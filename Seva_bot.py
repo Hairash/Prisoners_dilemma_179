b1a402d361f488335856ec07c7e25cb37313519d
unknown <vsevolod-pl@yandex.ru>
2019-02-27 19:39:00 +0300

    Fixed ZeroDivisionError

--- Seva_bot.py
+++ Seva_bot.py
@@ -1,8 +1,11 @@
 def Seva_bot(history,index,table):
+	from random import choice as ch
 	if index == 0:
 		ei = 1
 	else:
 		ei = 0
+	if len(history) == 0:
+		return ch(True,False)
 	eh = [history[i][ei] for i in range(len(history))]
 	me = sum(eh)/len(eh)
 	return me>0.5
