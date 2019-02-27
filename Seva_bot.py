5d8cc2611c4ce4f3a934061e4312186ac5ed3858
unknown <vsevolod-pl@yandex.ru>
2019-02-27 19:34:33 +0300

    Fixed IndentationError

--- Seva_bot.py
+++ Seva_bot.py
@@ -1,8 +1,8 @@
- def Seva_bot(history,index,table):
-    if index == 0:
-        ei = 1
-    else:
-    	ei = 0
-    eh = [history[i][ei] for i in range(len(history))]
-    me = sum(eh)/len(eh)
-    return me>0.5
+def Seva_bot(history,index,table):
+	if index == 0:
+		ei = 1
+	else:
+		ei = 0
+	eh = [history[i][ei] for i in range(len(history))]
+	me = sum(eh)/len(eh)
+	return me>0.5
