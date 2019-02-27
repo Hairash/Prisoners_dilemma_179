3b43bab0f6a5e52379d5ea856ddccb21d67fe512
Karapus <evgeny.nay@outlook.com>
2019-02-27 19:38:00 +0300

    Repared

--- evgfun.py
+++ evgfun.py
@@ -1,11 +1,15 @@
 def evgfun(history, num, table):
     
     def get_prob(des):
+        prob = 0
+        turns_amount = 0
         for turn in history:
-            dgkjbdkjvbkdjfvif turn[num] == des:
+            if (turn[num] == des):
                 prob += turn[not num]
-                turns_amoundt += 1
-        return (prob / turnsdf_amount)
+                turns_amount += 1
+        if (turns_amount != 0):
+            return (prob / turns_amount)
+        return 1
 
     def count_exp(des):
         return (get_prob(des) * (table[des][1][0] - table[des][1][1]) + (1 - get_prob(des)) * (table[des][0][0] - table[des][0][1]))
