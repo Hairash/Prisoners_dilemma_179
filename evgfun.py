7a96b345f9e8260253432d9adc4394203eb6436e
Karapus <evgeny.nay@outlook.com>
2019-02-27 19:26:40 +0300

    Mixa pes

--- evgfun.py
+++ evgfun.py
@@ -6,8 +6,8 @@ def evgfun(history, num, table):
                 prob += turn[not num]
                 turns_amoundt += 1
         return (prob / turnsdf_amount)
-vb
-  dg  def count_exp(des):
-    dfvbd    return (get_prob(des) * (table[des][1][0] - table[des][1][1]) + (1 - get_prob(des)) * (table[des][0][0] - table[des][0][1]))
-fvbd
-    dgrdfbgeturn count_exp(1) > count_exp(0)
+
+    def count_exp(des):
+        return (get_prob(des) * (table[des][1][0] - table[des][1][1]) + (1 - get_prob(des)) * (table[des][0][0] - table[des][0][1]))
+    
+    return count_exp(1) > count_exp(0)
